46bc2712d90c224d97edbf7580b46827f9912e08
tklltkioc <wusiyuong@gmail.com>
2019-03-16 22:06:18 +0800

    多种单例模式

--- Singleton.java
+++ Singleton.java
@@ -90,6 +90,10 @@ public class Singleton {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(Singleton1.getInstance()==Singleton1.getInstance());
+		System.out.println(Singleton2.getInstance()==Singleton2.getInstance());
+		System.out.println(Singleton3.getInstance()==Singleton3.getInstance());
+		System.out.println(Singleton4.getInstance()==Singleton4.getInstance());
+		System.out.println(Singleton5.getInstance()==Singleton5.getInstance());
 		System.out.println(Singleton6.INSTANCE==Singleton6.INSTANCE);
 
 	}
