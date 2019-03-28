85e2f32830594e32c5b44531279da3a55c9d1bab
tklltkioc <wusiyuong@gmail.com>
2019-03-28 10:34:33 +0800

    动归剪绳子

---
+++ CutRope.java
@@ -0,0 +1,35 @@
+package swordingTooffer;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class CutRope {
+    private static Hashtable<Integer,Long>sa=new Hashtable<>();
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while (sc.hasNext()){
+            int n=sc.nextInt();
+            if (n==2||n==3){
+                System.out.println(n-1);
+            }else {
+                System.out.println(dfs(n));
+            }
+        }
+        sc.close();
+    }
+    private static long dfs(int n){
+        if (n==1||n==2){
+            return n;
+        }
+        if (sa.containsKey(n))
+            return sa.get(n);
+        long max=n;
+        for (int i = 1; i < n/2; i++) {
+            max=Math.max(max,i*dfs(n-i));
+        }
+        sa.put(n,max);
+        return max;
+    }
+
+}
