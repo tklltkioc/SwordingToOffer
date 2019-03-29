86000fde66083ec6ccedf2c7921c2aacbcbb634a
tklltkioc <wusiyuong@gmail.com>
2019-03-29 18:46:35 +0800

    幂函数

---
+++ PowerByExponent.java
@@ -0,0 +1,38 @@
+package swordingTooffer;
+
+public class PowerByExponent {
+   static  double PowerWithExponent(double base,int exponent){
+       double res=1,curr=base;
+       int n;
+       if (exponent>0){
+           n=exponent;
+       }else if (exponent<0){
+           if (base==0)
+               throw new RuntimeException("分母为0");
+           n=-exponent;
+       }else {
+           return 1;
+       }
+       while (n!=0){
+           if ((n&1)==1)
+               res*=curr;
+           curr*=curr;
+           n>>=1;
+       }
+        return exponent>0?res:(1/res);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PowerWithExponent(2.0,4));
+        System.out.println(PowerWithExponent(-2.0,3));
+        System.out.println(PowerWithExponent(-2.0,-3));
+        System.out.println(PowerWithExponent(-2.0,0));
+        System.out.println(PowerWithExponent(2.0,-3));
+        System.out.println(PowerWithExponent(0,4));
+        System.out.println(PowerWithExponent(1,4));
+        System.out.println(PowerWithExponent(1,4));
+        System.out.println(PowerWithExponent(0,-3));
+
+    }
+
+}
