5fe8b4d0ee77d7651fce11e0c329c5093a9fe038
tklltkioc <wusiyuong@gmail.com>
2019-04-17 15:02:52 +0800

    全排列+解释

--- fullPermutation.java
+++ fullPermutation.java
@@ -1,4 +1,55 @@
 package swordingTooffer;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class fullPermutation {
-}
+        public static void main(String[] args) {
+            fullPermutation p = new fullPermutation();
+            System.out.println(p.Permutation( "abb").toString());
+        }
+
+        public static ArrayList<String> Permutation(String str) {
+            List<String> res = new ArrayList<>();
+            if (str != null && str.length() > 0) {
+                PermutationHelper(str.toCharArray(), 0, res);
+                Collections.sort(res);//顺序排列
+            }
+            return (ArrayList) res;
+        }
+
+        public static void PermutationHelper(char[] cs, int i, List<String> list) {
+            if (i == cs.length - 1) {
+                String val = String.valueOf(cs);
+                //去重
+                if (!list.contains(val))
+                    list.add(val);
+            } else {
+                for (int j = i; j < cs.length; j++) {
+
+                    //             a|b|c(状态A)
+                    //               |
+                    //               |swap(0,0)
+                    //               |
+                    //             a|b|c(状态B)
+                    //             /  \
+                    //   swap(1,1)/    \swap(1,2)  (状态C1和状态C2)
+                    //           /      \
+                    //         a|b|c   a|c|b
+                    swap(cs, i, j);
+                    PermutationHelper(cs, i + 1, list);
+                    // 第二个swap用以使得字符数组的顺序回到进入递归前的状态，这样才不会影响外部的遍历顺序。
+                    // 因为在第一次交换后进入递归运算的时候，字符数组的顺序改变了，
+                    // 例如“abc”， i = 1时对应‘b’，j = 2时对应 'c'，进行一次交换，此时的字符数组的顺序为 "acb"，从递归返回时，顺序依然是“acb”，
+                    // 则进行第二次交换使得 “acb” -> “abc”，这样在后续才可以进行递归交换，不会落下某一种情况。
+                    swap(cs, i, j);
+                }
+            }
+        }
+        public static void swap(char[] cs, int i, int j) {
+            char temp = cs[i];
+            cs[i] = cs[j];
+            cs[j] = temp;
+        }
+    }
