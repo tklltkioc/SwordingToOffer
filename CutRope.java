a21afdf45f70aeaad178e14347f1c014f4556c9a
tklltkioc <wusiyuong@gmail.com>
2019-09-16 11:12:53 +0800

    递归剪绳子代码

--- CutRope.java
+++ CutRope.java
@@ -3,35 +3,36 @@ package SwordingToOffer;
 import java.util.Hashtable;
 import java.util.Scanner;
 
+// 剪绳子
 public class CutRope {
-    private static Hashtable<Integer, Long> sa = new Hashtable<> ();
-
+    private static Hashtable<Integer, Long> map = new Hashtable<> ();
     public static void main (String[] args) {
         Scanner sc = new Scanner (System.in);
-        while (sc.hasNext ()) {
+        while (sc.hasNext ()){
             int n = sc.nextInt ();
-            if (n == 2 || n == 3) {
+            if (n == 2 || n == 3){
                 System.out.println (n - 1);
                 continue;
-            } else {
+            }else {
                 System.out.println (dfs (n));
             }
+
         }
         sc.close ();
     }
-
-    private static long dfs (int n) {
-        if (n == 1 || n == 2) {
+    private static long dfs(int n){
+        if (n == 1 || n == 2){
             return n;
         }
-        if (sa.containsKey (n))
-            return sa.get (n);
+        if (map.containsKey (n)){
+            return map.get (n);
+        }
         long max = n;
-        for (int i = 1; i <= n / 2; i++) {
+        for (int i = 1; i <= n/2 ; i++) {
+            // max与i和dfs(n-i)相乘进行数值比较，递归
             max = Math.max (max, i * dfs (n - i));
         }
-        sa.put (n, max);
+        map.put (n, max);
         return max;
     }
-
 }
