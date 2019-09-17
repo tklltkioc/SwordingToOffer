5f21ee2164e9c5b26f1428fb442f0042200ba595
tklltkioc <wusiyuong@gmail.com>
2019-09-17 10:08:40 +0800

    约瑟夫环代码

--- JosephuseRing.java
+++ JosephuseRing.java
@@ -1,14 +1,8 @@
 package SwordingToOffer;
-
+// 约瑟夫环
 public class JosephuseRing {
     public static int LastRemaining_Solution(int n, int m) {
         if (n == 0 || m == 0) return -1;
-//        int s=0;
-////        for(int i=2;i<=n;i++)
-////        {
-////            s=(s+m)%i;
-////        }
-////        return s ;
         return (LastRemaining_Solution (n - 1, m) + m) % n;
     }
 
