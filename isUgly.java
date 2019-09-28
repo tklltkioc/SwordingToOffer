fd307b32f8d7b2f8159437cb485669d42dc027c2
tklltkioc <wusiyuong@gmail.com>
2019-09-28 20:15:18 +0800

    添加计算丑数的注释

--- isUgly.java
+++ isUgly.java
@@ -1,5 +1,6 @@
 package SwordingToOffer;
 
+// 丑数计算
 public class isUgly {
     public static int GetUglyNumber_Solution (int index) {
         if (index <= 0)
@@ -14,7 +15,7 @@ public class isUgly {
         int tmp;
         while (count < index - 1) {
             tmp = min (result[i2] * 2, min (result[i3] * 3, result[i5] * 5));
-            if (tmp == result[i2] * 2) i2++;//三条if防止值是一样的，不要改成else的
+            if (tmp == result[i2] * 2) i2++;//三条if防止值是一样的
             if (tmp == result[i3] * 3) i3++;
             if (tmp == result[i5] * 5) i5++;
             result[++count] = tmp;
